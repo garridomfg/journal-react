b0d92d3b54b22e9fce6fb73bad561a7e49920302
mariano <garridomfg@gmail.com>
2023-09-11 14:13:24 +0200

    Develop main layout mock

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,18 +1,27 @@
-import { Typography } from "@mui/material";
+import { IconButton } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
 import { JournalLayout } from "../layouts/JournalLayout";
-
+import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
-        Ut consequat voluptate culpa Lorem occaecat nostrud fugiat ullamco aute.
-        Culpa Lorem id sit ipsum ullamco do. Laborum sunt non qui et est esse
-        amet in laborum veniam aute. Sunt veniam ad cupidatat deserunt culpa
-        ullamco et aliqua ex. Amet irure sit incididunt velit. Lorem ea anim
-        nulla excepteur fugiat et ex dolore aute laboris. Tempor consectetur
-        exercitation amet adipisicing ad cupidatat.
-      </Typography>
+      <NothingSelectedView />
+      <NoteView />
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
